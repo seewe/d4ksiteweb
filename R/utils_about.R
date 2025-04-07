ee50f6236d9b3e0ec265d9862ab89331efb7b5d8
seewe <eebouele@gmail.com>
2025-04-06 23:18:59 -0400

    about section write within cards

--- R/utils_about.R
+++ R/utils_about.R
@@ -6,113 +6,104 @@
 
 about_skills <- list(
   coding = list(
-    icon = "file-code",
+    icon = "fas fa-laptop-code",
     title = "CODING",
     description = data.frame(
-      item = c("R", "Python", "HtmlCssJs", "java", "SQL"),
-      icon = c("code", "code", "code", "code", "code"),
-      color = c("primary", "warning", "danger", "danger", "info"),
-      text = c(
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur"
-      )
-    ) |>
-      dplyr::mutate(
-        text_id = stringr::str_to_lower(paste0(
-          stringr::str_split(item, "-", simplify = TRUE)[, 1],
-          dplyr::row_number()
-        ))
+      item = c("R", "Python", "HtmlCssJs", "Java", "SQL"),
+      icon = c(
+        "fab fa-r-project",
+        "fab fa-python",
+        "fab fa-html5",
+        "fab fa-java",
+        "fas fa-code"
       ),
+      color = c("primary", "warning", "danger", "danger", "info")
+    ),
     expert_name = "Valère",
     expert_description = "The Developper",
     expert_profil = "https://mdbootstrap.com/img/new/standard/nature/184.jpg",
     expert_link = "#!/developer"
   ),
   database = list(
-    icon = "database",
+    icon = "fas fa-database",
     title = "DATABASE",
     description = data.frame(
-      item = c("Microsoft-Azure", "Oracle", "MySQL", "Neo4J", "Mongodb"),
-      icon = c("database", "database", "database", "database", "database"),
-      color = c("info", "danger", "primary", "primary", "success"),
-      text = c(
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur"
-      )
-    ) |>
-      dplyr::mutate(
-        text_id = stringr::str_to_lower(paste0(
-          stringr::str_split(item, "-", simplify = TRUE)[, 1],
-          dplyr::row_number()
-        ))
+      item = c("Azure synapse", "Oracle", "MySQL", "Neo4J", "Mongodb"),
+      icon = c(
+        "fas fa-database",
+        "fas fa-database",
+        "fas fa-database",
+        "fas fa-database",
+        "fas fa-database"
       ),
+      color = c("info", "danger", "primary", "primary", "success")
+    ),
     expert_name = "Sergio",
     expert_description = "The Storage",
     expert_profil = "https://mdbootstrap.com/img/new/standard/nature/185.jpg",
     expert_link = "#!/storage"
   ),
   modeling = list(
-    icon = "brain",
+    icon = "fas fa-brain",
     title = "MODELING",
     description = data.frame(
       item = c(
         "LM-GLM",
         "Trees-models",
-        "Boosting-method",
-        "Neural-network",
+        "Boosting",
+        "Neural-net",
         "LLM-models"
       ),
-      icon = c("chart-line", "sitemap", "sitemap", "sitemap", "sitemap"),
-      color = c("primary", "success", "success", "info", "primary"),
-      text = c(
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur"
-      )
-    ) |>
-      dplyr::mutate(
-        text_id = stringr::str_to_lower(paste0(
-          stringr::str_split(item, "-", simplify = TRUE)[, 1],
-          dplyr::row_number()
-        ))
+      icon = c(
+        "fas fa-chart-line",
+        "fas fa-sitemap",
+        "fas fa-sitemap",
+        "fas fa-sitemap",
+        "fas fa-sitemap"
       ),
+      color = c("primary", "success", "success", "info", "primary")
+    ),
     expert_name = "Elvis",
     expert_description = "The Modeler",
     expert_profil = "https://mdbootstrap.com/img/new/standard/nature/186.jpg",
     expert_link = "#!/modeler"
   ),
   sharing = list(
-    icon = "square-share-nodes",
+    icon = "fas fa-square-share-nodes",
     title = "SHARING",
     description = data.frame(
       item = c("API", "Dashboards", "Website", "MobileApp", "Books"),
-      icon = c("share-alt", "desktop", "desktop", "mobile", "book"),
-      color = c("primary", "warning", "primary", "success", "dark"),
-      text = c(
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur"
-      )
-    ) |>
-      dplyr::mutate(
-        text_id = stringr::str_to_lower(paste0(
-          stringr::str_split(item, "-", simplify = TRUE)[, 1],
-          dplyr::row_number()
-        ))
+      icon = c(
+        "fas fa-share-alt",
+        "fas fa-desktop",
+        "fas fa-desktop",
+        "fas fa-mobile",
+        "fas fa-book"
       ),
+      color = c("primary", "warning", "primary", "success", "dark")
+    ),
     expert_name = "Vanessa",
     expert_description = "The Communicator",
     expert_profil = "https://mdbootstrap.com/img/new/standard/nature/187.jpg",
     expert_link = "#!/publisher"
+  ),
+  cloud = list(
+    icon = "fas fa-cloud",
+    title = "CLOUD",
+    description = data.frame(
+      item = c("Azure", "AWS", "Google-Cloud", "Posit-Cloud", "Digital-Ocean"),
+      icon = c(
+        "fab fa-buysellads",
+        "fab fa-aws",
+        "fab fa-google",
+        "fas fa-cloud",
+        "fab fa-digital-ocean"
+      ),
+      color = c("primary", "dark", "danger", "dark", "primary")
+    ),
+    expert_name = "Guillaume",
+    expert_description = "The Clouder",
+    expert_profil = "https://mdbootstrap.com/img/new/standard/nature/188.jpg",
+    expert_link = "#!/clouder"
   )
 )
